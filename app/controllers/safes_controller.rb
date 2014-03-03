bf1f049638ee192782028494c28b578dae84d29c
relotnek <kentoler@Kenneths-MacBook-Pro.local>
2014-03-03 13:45:13 -0500

    added key generation to users controller

--- app/controllers/safes_controller.rb
+++ app/controllers/safes_controller.rb
@@ -1,2 +1,19 @@
 class SafesController < ApplicationController
+before_filter :authenticate_user!
+
+def index
+end
+
+def new
+end
+
+def create
+end
+
+def show
+end
+
+def edit
+end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  private
+  def generate
+    @user = current_user
+    @user.priv_key = RbNaCl::PrivateKey.generate
+    @user.public_key = @user.privkey.public_key
+    @user.save
+  end
 end
