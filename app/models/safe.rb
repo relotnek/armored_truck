70ac35ab85637159140393d178c97d34d58785ea
Ken Toler <ken.toler@nvisiumsecurity.com>
2014-03-04 15:32:11 -0500

    added associations between users and safes

--- app/models/safe.rb
+++ app/models/safe.rb
@@ -1,3 +1,4 @@
 class Safe < ActiveRecord::Base
+	belongs_to :user
 end
 	
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304185424) do
+ActiveRecord::Schema.define(version: 20140304202956) do
 
   create_table "safes", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140304185424) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "rawfile"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
