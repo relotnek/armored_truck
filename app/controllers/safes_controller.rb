b03767e7003c542043d076f5ca6822f55e49f0d2
Ken Toler <ken.toler@nvisiumsecurity.com>
2014-03-04 15:16:30 -0500

    encryption working

--- app/controllers/safes_controller.rb
+++ app/controllers/safes_controller.rb
@@ -30,8 +30,17 @@ def upload
   @user = current_user
   uploaded_io = params[:safe][:rawfile]
   File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-    file.write(uploaded_io.read)
+
+    box = RbNaCl::Box.new(@user.priv_key, @user.public_key)
+    @nonce = RbNaCl::Random.random_bytes(box.nonce_bytes)
+    message = uploaded_io.read
+    ciphertext = box.encrypt(@nonce,message)
+    File.open(Rails.root.join('public','uploads', "vt-#{uploaded_io.original_filename}"), 'wb') do |noncefile|
+      noncefile.write(@nonce)
+    end
+    file.write(ciphertext)
   end
+
   redirect_to safes_path
 end
 
