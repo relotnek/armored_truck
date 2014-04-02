aaa578c91bde7238d97142542f71782c28c63066
relotnek <kentoler@Kenneths-MacBook-Pro.local>
2014-04-02 16:59:09 -0400

    added authenticate filter to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @users = User.all
   end
 
   def show
   end
-  
+
   def generate
     @user = current_user
     private = RbNaCl::PrivateKey.generate
