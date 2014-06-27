20d783209bd390df1f31eb4d471cb4d27378c54c
Ken T <kentoler@gmail.com>
2014-06-27 14:11:59 -0700

    removed legacy key generation from edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,5 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @keys = @user.keys.all
   end
-
-  def generate
-    @user = current_user
-    private = RbNaCl::PrivateKey.generate
-    public = private.public_key
-    @user.public_key = public.to_bytes
-    @user.save
-    send_data private, :filename => "ac_privkey"
-  end
+  
 end
