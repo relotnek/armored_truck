44994f29ebeec0a29311e5d4079fde3cf7b0e6f7
relotnek <ken@kentoler.com>
2014-09-06 12:34:29 -0400

    removed extra columns from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626152319) do
+ActiveRecord::Schema.define(version: 20140906163006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,9 +47,6 @@ ActiveRecord::Schema.define(version: 20140626152319) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "name"
-    t.binary   "priv_key"
-    t.binary   "public_key"
-    t.binary   "public"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
