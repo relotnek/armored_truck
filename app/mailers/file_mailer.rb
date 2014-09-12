baba52f99e5c42011c290e216b86d450a0dba2b7
Ken T <kentoler@gmail.com>
2014-09-12 13:47:43 -0400

    added attachment to mail - broken on CRLF though

--- app/mailers/file_mailer.rb
+++ app/mailers/file_mailer.rb
@@ -1,9 +1,14 @@
 class FileMailer < ActionMailer::Base
   default from: "armor@armoredtruck.com"
 
-  def mail_file(recipient)
+  def mail_file(recipient, sender, pubkey, privkeyname, encryptedfile, encfilename)
     @recipient = recipient
+    @sender = sender
+    @pubkeyname = pubkey
+    @privkeyname = privkeyname
+    attachments[encfilename] = {encoding: 'quoted-printable', content: encryptedfile }
     mail(to: @recipient, subject: "ARMOR!")
+
   end
 
 end
